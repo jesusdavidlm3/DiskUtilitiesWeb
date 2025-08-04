a432c11f91219bc717cfda1a842d8a8644a34d7e
Jesus David Lozano <jesusdavidlozanomarin@gmail.com>
2025-08-03 23:22:18 -0400

    Ultima prueba ad sense.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,6 +3,7 @@ export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-4000113355050688"></meta>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>diskUtilities</title>
